cc05d2e7b0837d3cbdc86e92af221f455e929808
muhammet bulut <69051369+bulut67@users.noreply.github.com>
2020-09-11 20:45:49 +0300

    Add files via upload

---
+++ FhcTripHotelPage.java
@@ -0,0 +1,32 @@
+package com.techproed.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class FhcTripHotelPage {
+    WebDriver driver;
+    public FhcTripHotelPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(id = "Code")
+    public WebElement kodKutusu;
+    @FindBy (id = "Name")
+    public WebElement nameKutusu;
+    @FindBy (id = "Address")
+    public WebElement adresKutusu;
+    @FindBy (id = "Phone")
+    public WebElement telefonKutusu;
+    @FindBy (id = "Email")
+    public WebElement emailKutusu;
+    @FindBy (id = "IDGroup")
+    public WebElement selectKutusu;
+    @FindBy (id = "btnSubmit")
+    public WebElement saveButonu;
+
+
+
+}
